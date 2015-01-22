c37581f78af5f82bd30b5eec0d6ffef07e47ebdc
Carolina Powers <carolinapoloni@gmail.com>
2015-01-22 14:36:58 -0500

    last commit before merging

--- scripts/main.js
+++ scripts/main.js
@@ -38,4 +38,51 @@ assert.equal(div(10,5),2);
 console.log(div(6,2));
 
 
+var one = "one"
+one = 1
+
+function sum (a, b) {
+return a + b;
+}
+
+console.log(sum("one" + "one"));
+
+var assert = require("assert")
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})
+
+
+
+/* @param String A representing an English number word
+* @param String B representing an English number word
+* @return Number*/
+
+function plus (A,B) {
+    if ( A == "zero") {
+        return B;   
+    }
+    return 2;
+}
+
+it('should add "zero" and "zero"', function(){
+    assert.equal(plus("zero", "zero"), 0);});
+
+
+function plus (A,B) {
+    if (A + B) {
+        return 2; 
+    }
+}
+    
+it('should add number words', function(){
+    assert(plus("one", "one") === 2);});
+
+
+
 
