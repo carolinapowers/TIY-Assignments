974712f5aae3ed3a53ad80175d689df825febceb
Carolina Powers <carolinapoloni@gmail.com>
2015-01-25 22:24:38 -0500

    chess pieces moved with functions

--- scripts/main.js
+++ scripts/main.js
@@ -12,16 +12,32 @@ var board = [
 
 
 
+function print(board){
+    console.log(board.join('\n') + '\n\n');
+}
 
-console.log(board.join('\n') + '\n\n');
+print(board);
 
-//Move King's Pawn forward 2
+
+ function move(board, fromX, fromY, toX, toY){
+  board [toX][toY] = board [fromX][fromY];
+  board[fromX][fromY] = ' ';
+  console.log(board.join('\n') + '\n\n');
+}
+
+move(board,6,4,4,4);
+
+move(board,1,3,3,3);
+
+move(board,7,6,5,7);  
+
+/*Move King's Pawn forward 2
 
 board [4][4] = board [6][4];
 board[6][4] = ' ';
 console.log(board.join('\n') + '\n\n');//
 
-//Move Q's P forward 2
+/*Move Q's P forward 2
 board [3][3] = board [1][3];
 board[1][3] = ' ';
 console.log(board.join('\n') + '\n\n');
@@ -29,4 +45,4 @@ console.log(board.join('\n') + '\n\n');
 //Move right n foward 2 right 1
 board[5][7] = board [7][6];
 board [7][6] = ' ';
-console.log(board.join('\n') + '\n\n');
+console.log(board.join('\n') + '\n\n');*/
