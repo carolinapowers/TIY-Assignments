19ca5d040a074f8d958d9bfc35d4bff95a085fda
Carolina Powers <carolinapoloni@gmail.com>
2015-01-25 21:26:57 -0500

    string calculator with && and ||

--- scripts/string-calculator.js
+++ scripts/string-calculator.js
@@ -14,7 +14,7 @@ it('should add "zero" and "three"', function () {
 
 */
 
-function plus (A,B) {
+/* function plus (A,B) {
     if(A == "one") {
         return 5;
     }
@@ -39,6 +39,82 @@ it('should add "one" and "ten"', function(){
     assert.equal(plus("one", "ten"), 5);
 })
 
-it ('should add "one" and "one"', function (){
+it('should add "one" and "two"', function() {
+    assert.equal(plus("one", "two"), 5);
+})
+
+it('should add "one" and "one"', function (){
     assert.equal(plus("one","one"), 5);
+})
+
+it('should add "one" and "three"', function () {
+    assert.equal(plus("one","three"),5 );
+})
+
+it('should add "one" and "five"', function(){
+    assert.equal(plus("one","three"),5);
+})
+
+it('should add "one" and "seven"', function(){
+    assert.equal(plus("one","seven"),5);
+})
+
+it('should add "one" and "nine"', function(){
+    assert.equal(plus("one","nine"),5);
+})*/
+
+/*function plus (A,B) {
+    if(A=="two"||B=="two"){
+    return 1;    
+    }        
+}
+
+it('should add "one" and "two"', function(){
+    assert.equal(plus("one","two"), 1);
+})
+
+it('should add "two" and "one"', function(){
+    assert.equal(plus("two","one"),1);
+})
+
+it('should add "two" and "three"',function(){
+    assert.equal(plus("two","three"), 1);
+})
+
+it('should add "two" and "four"', function(){
+    assert.equal(plus("two","four"),1);
+})
+
+it('should add "two" and "five"', function(){
+    assert.equal(plus("two","five"), 1);
+})
+
+it('should add "two" and "six"', function(){
+    assert.equal(plus("two", "six"), 1);
+})
+
+it('should add "seven" and "two"', function(){
+    assert.equal(plus("seven", "two"),1);
+})
+
+it('should add "eight" and "two"',function(){
+    assert.equal(plus("eight","two"), 1);
+})
+
+it('should add "eight" and "two"', function(){
+    assert.equal(plus("eight", "two"), 1);
+})
+
+it('should add "nine" and "two"', function(){
+    assert(plus("nine","two"),1);
+})*/
+
+function plus(A,B) {
+    if(A=="three"&&B=="three") {
+        return 6;
+    }
+}
+
+it('should be "three" and "three"', function(){
+    assert.equal(plus("three","three"),6);
 })
