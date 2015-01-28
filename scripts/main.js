40244a73c5776cb13ea1f6b55ad5beee6df55044
Carolina Powers <carolinapoloni@gmail.com>
2015-01-28 18:00:32 -0500

    commented out old chessboard

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,39 @@
-var board = [
-    ['R','N','B','Q','K','B','N','R'],
-    ['P','P','P','P','P','P','P','P'],
-    [' ',' ',' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ',' ',' '],
-    ['p','p','p','p','p','p','p','p'],
-    ['r','n','b','q','k','b','n','r'] ];
 
 
 
 
-
-function print(board){
-    console.log(board.join('\n') + '\n\n');
-}
-
-print(board);
-
-
- function move(board, fromX, fromY, toX, toY){
-  board [toX][toY] = board [fromX][fromY];
-  board[fromX][fromY] = ' ';
-  console.log(board.join('\n') + '\n\n');
-}
-
-move(board,6,4,4,4);
-
-move(board,1,3,3,3);
-
-move(board,7,6,5,7);  
+//var board = [
+//    ['R','N','B','Q','K','B','N','R'],
+//    ['P','P','P','P','P','P','P','P'],
+//    [' ',' ',' ',' ',' ',' ',' ',' '],
+//    [' ',' ',' ',' ',' ',' ',' ',' '],
+//    [' ',' ',' ',' ',' ',' ',' ',' '],
+//    [' ',' ',' ',' ',' ',' ',' ',' '],
+//    ['p','p','p','p','p','p','p','p'],
+//    ['r','n','b','q','k','b','n','r'] ];
+//
+//
+//
+//
+//
+//function print(board){
+//    console.log(board.join('\n') + '\n\n');
+//}
+//
+//print(board);
+//
+//
+// function move(board, fromX, fromY, toX, toY){
+//  board [toX][toY] = board [fromX][fromY];
+//  board[fromX][fromY] = ' ';
+//  console.log(board.join('\n') + '\n\n');
+//}
+//
+//move(board,6,4,4,4);
+//
+//move(board,1,3,3,3);
+//
+//move(board,7,6,5,7);  
 
 /*Move King's Pawn forward 2
 
