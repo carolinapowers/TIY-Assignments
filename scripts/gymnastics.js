ace6d10ada80e219dc5c5a299c41583b3f0c4437
Carolina Powers <carolinapoloni@gmail.com>
2015-01-28 14:28:37 -0500

    adding package.json file

--- scripts/gymnastics.js
+++ scripts/gymnastics.js
@@ -1,100 +1,39 @@
 var events = require('../apis/github/users/carolinapowers/events.json');
 var assert = require('assert');
+console.log(Date());
 
-
-it('should have events', function(){
-    assert(events);
-});
-
-
-function answer () {
-    var pushEvents = events.filter(function(item){
-        return item.type == 'PushEvent';
-    })
-    return {
-        'total': events.length,
-        'PushEvent': {
-            'total': pushEvents.length
-               
-        },
-        other:{
-            'total': answer.total - answer.PushEvent.total
-        
-    }
-    }
-}
-
-console.log(answer.total - answer.PushEvent.total);
-console.log(answer());
-
-
-var theAnswer = answer();
-
-it('should return that answer exists, and test for total events', function (){
-    assert(answer);
-    assert(events.length == 30);
-})
-
-it('should return the length when calling answer function', function(){
-    assert.equal(theAnswer.total, 30);
-})
-
-it('should have "PushEvent", and has a total count of PushEvents', function() {
-   assert(theAnswer.PushEvent);
-   assert(theAnswer.PushEvent.total);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function answer () {
-    return {
-        'total': events.length
-    }
-} //end answer
-
-
-/*describe('the setup', function(){
-    it('should have events', function(){
+    it('should tell us that events has an input', function (){
         assert(events);
     });
 
-
-    it('should have events', function(){
-        assert(answer);
-        assert(typeof answer == 'function');
+function answer(){
+    var pushEvents = events.filter(function(item){
+        return item.type == 'PushEvent';
     });
-});
-
-
-describe('the answer', function(){
-    var theAnswer = answer();
     
-it('should have 30 total events', function() {
-    assert(theAnswer.total === 30);
-})    
+    return { 
+        'total': events.length,
+        'PushEvent': {
+            'total': pushEvents.length,
+        }
+    };
+}
 
-it('should have some "PushEvent" entries', function() {
-    assert(theAnswer.PushEvents.total);
-});
 
-});
-*/
+console.log(answer());
+console.log(events.length);
+var theAnswer = answer();
+
+    it('should return that answer exists, and test for total events', function(){
+        assert(answer);
+        assert(events.length === 30);
+    });
+
+    it('should return the length when calling answer function', function(){
+       assert.equal(theAnswer.total, 30); 
+    });
+
+    it('should have "PushEvent", and has a total count of PushEvents', function(){
+        assert(theAnswer.PushEvent);
+        assert(theAnswer.PushEvent.total);
+    });
