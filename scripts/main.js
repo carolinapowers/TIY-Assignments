bcb9d576747e44c7a612ee9046dbf9bbe3f5e14b
Carolina Powers <carolinapoloni@gmail.com>
2015-01-28 20:31:38 -0500

    attempting to create rows

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,31 @@
+var _ = require ('lodash')
 
+
+
+function print(board){
+    
+var row = [7];
+    
+  
+  console.log('<table class="chessboard">');
+
+  _.forEach(board, function(row, rank){
+
+      // Before any cells are printed...
+
+      _.forEach(row, function(square, file){
+
+          // In here, we have `rank`, `file`, and `square`...
+
+      }); // END foreach(row)
+
+      // After all the cells are printed...
+
+  }); // END foreach(board)
+
+  // After all the rows are printed...
+  console.log('</table> <!-- .chessboard -->');
+} // END print
 
 
 
