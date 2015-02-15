7c00952b6c8e30a951b8dad9814f4728c35eb0d0
Carolina Powers <carolinapoloni@gmail.com>
2015-02-15 11:46:52 -0500

    total events

--- apis/gymnastics.js
+++ apis/gymnastics.js
@@ -1,3 +1,30 @@
 var assert = require('assert'),
     _ = require('lodash'),
-var events = require('../apis/github/users/carolinapowers/events.json')
+    events = require('../apis/github/users/carolinapowers/events.json');
+
+function answer(){
+  return {
+    'total': events.length,     // How many total events did you fetch?
+    'PushEvent': {
+      'total': "",  // How many total events of type `PushEvent` are there?
+      'perDay': "", // On average, how many`PushEvent` entries per day?
+    },
+    'other': {
+      'total': "",   // How many _other_ events are in the data?
+      'perDay': "",  // How many per day, on average?
+    }
+  };
+} 
+
+var theAnswer = answer();
+
+it('should assert that "answer" exists', function(){
+   assert(answer); 
+    
+});
+
+it('should assert that "theAnswer.total" equals "events.length" exists', function(){
+   assert(theAnswer.total == events.length);    
+});
+
+
