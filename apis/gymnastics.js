eccebb63ef3d6dca9364c2acbe7f7ab9c11badd9
Carolina Powers <carolinapoloni@gmail.com>
2015-02-15 13:54:02 -0500

    totalDays

--- apis/gymnastics.js
+++ apis/gymnastics.js
@@ -2,16 +2,34 @@ var assert = require('assert'),
     _ = require('lodash'),
     events = require('../apis/github/users/carolinapowers/events.json');
 
-function answer(){
-    var pushEvents = _.filter(events, {
-            'type': 'PushEvent'
+
+
+var pushEvents = _.filter(events, {
+        'type': 'PushEvent'
     });
     console.log(pushEvents.length);
-    
+
+var first = _.first(events);
+var last = _.last(events);
+
+var firstDay = first.created_at;
+console.log(firstDay);
+
+var lastDay = last.created_at;
+console.log(lastDay);
+
+var totalDays = lastDay - firstDay;
+console.log(totalDays);
+
+//var totalDays = last.created_at - first.created_at;
+//console.log(first);
+//console.log(last);
+
+function answer(){
     return {
     'total': events.length,     // How many total events did you fetch?
     'PushEvent': {
-      'total': "",  // How many total events of type `PushEvent` are there?
+      'total': pushEvents.length,  // How many total events of type `PushEvent` are there?
       'perDay': "", // On average, how many`PushEvent` entries per day?
     },
     'other': {
@@ -36,5 +54,13 @@ it('should assert that "theAnswer.total" equals "events.length" exists', functio
 it('should assert that "events.length" equals 30', function () {
    assert.equal((events.length), 30); 
 });
+
+it('should assert that total of PushEvent equals pushEvents.length', function () {
+    assert(theAnswer.PushEvent.total == pushEvents.length);
+})
+
+it('should assert that "pushEvents.length" equals 24', function () {
+    assert.equal((pushEvents.length), 24);
+})
 
 
