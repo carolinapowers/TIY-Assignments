ac50b1421f33742b75e93dc45e310513f781cedd
Carolina Powers <carolinapoloni@gmail.com>
2015-02-15 18:44:47 -0500

    i couldn't figure out how to assign ids for tds, so i went the long way with Vue

--- chessboard/js/main.js
+++ chessboard/js/main.js
@@ -4,6 +4,12 @@
   el: '#chessboard',
    data: {
      message: 'Hello from Vue'
-
-  },
+   },
  });
+
+var vm = new Vue ({
+    el: '#row',
+    data: {
+    HTMLTableDataCellElement: 'Hello'
+    },
+});
