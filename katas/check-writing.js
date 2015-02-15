68dd7f1d4dbc86247ab6e26c5a9b218e6e6e4d4f
Carolina Powers <carolinapoloni@gmail.com>
2015-02-14 22:59:35 -0500

    check-writing with function toEnglish and else if

---
+++ katas/check-writing.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+
+function toEnglish(a) {
+    if (a == 1) {
+        return "one";
+    } else if (a== 2) {
+        return "two";
+    } else if (a==3) {
+        return "three";
+    } else if (a==4) {
+        return "four";
+    } else if (a==5) {
+        return "five";
+    } else if (a==6) {
+        return "six";
+    } else if (a==7){
+        return "seven";
+    } else if (a==8){
+        return "eight";
+    } else if (a==9) {
+        return "nine"; 
+    } else if (a==10) {
+        return "ten";
+    }
+    
+}
