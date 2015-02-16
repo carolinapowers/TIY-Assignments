cdede9b2a2944c7339e917d2ecbc265d105e6bc9
Carolina Powers <carolinapoloni@gmail.com>
2015-02-15 23:08:07 -0500

    tried a bunch of stuff, but i couldn't move a piece...that's it...good night

--- chessboard/js/main.js
+++ chessboard/js/main.js
@@ -7,9 +7,20 @@
    },
  });
 
-var vm = new Vue ({
-    el: '#row',
-    data: {
-    HTMLTableDataCellElement: 'Hello'
-    },
-});
+
+
+document.write('<div class="button-group">');
+    document.write('<i class="fa fa-angle-double-left fa-lg" onClick="rewind()"></i>');
+    document.write('<i class="fa fa-angle-left fa-lg" onClick="movePieceBack()"></i>');
+    document.write('<i class="fa fa-angle-right fa-lg" onClick="movePiece()"></i>');
+    document.write('<i class="fa fa-angle-double-right fa-lg" onclick="fastForward()"></i>');
+    document.write('</div>');
+
+
+//$("#row6-1" ).replaceWith( "#row7-0");
+
+//$("#row8-0:before").css();
+
+var lContent = document.getElementById('#row7-0').innerHTML;
+        document.getElementById('#row7-0').innerHTML = "";
+        document.getElementById('#row6-0').innerHTML = lContent;
