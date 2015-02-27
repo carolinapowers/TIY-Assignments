5259af110c195c4fbfa8c50bd81feb42b2014be0
Carolina Powers <carolinapoloni@gmail.com>
2015-02-26 23:21:12 -0500

    submit button is working... do not forget to pass the parameter next time

--- pub/js/app.js
+++ pub/js/app.js
@@ -27,8 +27,8 @@
     app.controller('ReviewController', function () {
         this.review = {};
 
-        this.addReview = function () {
-            product.reviews.push(this.review)
+        this.addReview = function (product) {
+            product.reviews.push(this.review);
             this.review = {};
             this.review.createdOn = Date.now();
         };
